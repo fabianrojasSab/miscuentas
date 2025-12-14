1883702a7ae373541948284e8d16139c5eb78a66
Fabian Rojas <fabianrojas.sab@gmail.com>
2025-12-14 12:54:43 -0500

    feat: api
    Se crean los puertos para realizar login y registrarse en el sistema

---
+++ src/pages/api/login.ts
@@ -0,0 +1,41 @@
+import { login } from "@/lib/db/queries/users";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Método no permitido" });
+    }
+
+    try {
+        const { email, password} = req.body as {
+        email?: string;
+        password?: string;
+        };
+
+        if (!email || !password) {
+            return res
+            .status(400)
+            .json({ error: "Faltan campos obligatorios (Correo, Contraseña)" });
+        }
+
+        const user = await login({ email, password });
+
+        return res.status(200).json({
+            success: true,
+            user,
+        });
+    } catch (err: any) {
+        if (err.code === "SQLLITE_ERROR") {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (!err.code) {
+            return res.status(401).json({ error: err.message });
+        }
+
+        return res.status(500).json({ error: err.message });
+    }
+}
---
+++ src/pages/api/signup.ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createUser } from "@/lib/db/queries/users";
+import bcrypt from "bcryptjs";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Método no permitido" });
+    }
+
+    try {
+        const { name, email, password, passwordConfirm } = req.body as {
+        name?: string;
+        email?: string;
+        password?: string;
+        passwordConfirm?: string;
+        };
+
+        if (!name || !email || !password) {
+            return res
+                .status(400)
+                .json({ error: "Faltan campos obligatorios (nombre, correo, password)" });
+        }
+
+        if (password !== passwordConfirm) {
+            return res
+                .status(400)
+                .json({ error: "Las contraseñas no coinciden" });
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        await createUser({ name, email, passwordHash });
+
+        // Redirige al login después de registrarse
+        // res.writeHead(302, { Location: "/auth/logIn" });
+        // res.end();
+    } catch (err: any) {
+        if (err.code === "SQLLITE_ERROR") {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (!err.code) {
+            return res.status(401).json({ error: err.message });
+        }
+
+        return res.status(500).json({ error: err.message });
+    }
+}
