7c46e9b486444d0d0e64eecc02ce119ee23c9f7c
Fabian Rojas <fabianrojas.sab@gmail.com>
2025-12-14 13:07:53 -0500

    feat: login & register
    Se realiza modificacion en las vistar para implementar funcion respectivamente

--- src/pages/auth/logIn.tsx
+++ src/pages/auth/logIn.tsx
@@ -1,5 +1,68 @@
+import { Button } from "@/components/buttons";
+import { Header } from "@/components/header";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function LogIn() {
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const body = {
+            email: form.email.value,
+            password: form.password.value,
+        };
+
+        try {
+            const res = await fetch("/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            });
+            const data = await res.json();
+
+            if (!res.ok) {
+                setError(data.error);
+                return;
+            }
+
+            localStorage.setItem("user", JSON.stringify(data.user));
+
+            if(data.user.sw_admin === 1){
+                router.push("/admin/dashboard");
+            }else{
+                router.push("/user/dashboard");
+            }
+
+        } catch (err) {
+            setError("Error al iniciar sesión. Por favor, inténtalo de nuevo.");
+            setTimeout(() => setError(null), 5000);
+            form.reset();
+        }
+    }
+
     return (
-        <div>login</div>
+        <div>
+            <Header />
+            <div className="relative z-10 w-[45%] flex flex-col justify-center items-center px-16">
+                <h1 className="text-5xl font-bold text-gray-900 mb-10">Iniciar sesion</h1>
+                <form onSubmit={handleSubmit}>
+                    <Input type="email" name="email" placeholder="Correo electronico" className="mb-4"/>
+                    <Input type="password" name="password" placeholder="Contraseña" className="mb-4"/>
+
+                    {error && (
+                        <p className="text-red-600 text-center">{error}</p>
+                    )}
+
+                    <Button> Ingresar</Button>
+                </form>
+            </div>
+        </div>
     )
 }
--- src/pages/auth/signUp.tsx
+++ src/pages/auth/signUp.tsx
@@ -1,5 +1,59 @@
+import { Button } from "@/components/buttons";
+import { Header } from "@/components/header";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 export default function SignUp() {
+    const [error, setError] = useState<string | null>(null);
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setError(null);
+
+        const form = e.currentTarget;
+
+        const body = {
+            name: form.name.value,
+            email: form.email.value,
+            password: form.password.value,
+            passwordConfirm: form.passwordConfirm.value,
+        };
+
+        const res = await fetch("/api/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!res.ok) {
+            const data = await res.json();
+            setError(data.error);
+            return;
+        }
+
+        window.location.href = "/auth/logIn";
+    }
+
     return (
-        <div>registrarse</div>
+        <div>
+            <Header />
+            <div className="relative z-10 w-[45%] flex flex-col justify-center items-center px-16">
+                <h1 className="text-5xl font-bold text-gray-900 mb-10">Registrarse</h1>
+                <form onSubmit={handleSubmit}>
+                    <Input type="text" name="name" placeholder="Nombre completo" className="mb-4"/>
+                    <Input type="email" name="email" placeholder="Correo electronico" className="mb-4"/>
+                    <Input type="password" name="password" placeholder="Contraseña" className="mb-4"/>
+                    <Input type="password" name="passwordConfirm" placeholder="Confirmar contraseña" className="mb-4"/>
+                    
+                    {error && (
+                        <p className="text-red-600 text-center">{error}</p>
+                    )}
+        
+                    <Button type="submit"> Registrarse</Button>
+                </form>
+            </div>
+        </div>
     )
 }
