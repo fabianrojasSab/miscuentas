09da60defebdb7e2a2ea94fa9d46ef822626f18e
Fabian Rojas <fabianrojas.sab@gmail.com>
2026-08-23 02:25:54 -0500

    feat: tag google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,18 @@ export default function Document() {
     <Html lang="es">
       <Head />
       <body className="antialiased">
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-MRT465Q3"
+                height="0"
+                width="0"
+                style={{
+                    display: "none",
+                    visibility: "hidden",
+                }}
+            />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
