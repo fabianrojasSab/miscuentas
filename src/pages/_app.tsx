ae35208bcfe3dcd645b1b7aa491f1442c29cd0ab
Fabian Rojas <fabianrojas.sab@gmail.com>
2026-08-23 02:46:23 -0500

    hotfix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,46 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import Script from "next/script";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-      id="google-tag-manager"
-      strategy="afterInteractive">
-        {`function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MRT465Q3');`}
-      </Script>
-    
-    </>
-  )
+export default function App({
+    Component,
+    pageProps,
+}: AppProps) {
+    return (
+        <>
+            <Script
+                id="google-tag-manager"
+                strategy="afterInteractive"
+            >
+                {`
+                    (function(w,d,s,l,i){
+                        w[l]=w[l]||[];
+                        w[l].push({
+                            'gtm.start': new Date().getTime(),
+                            event:'gtm.js'
+                        });
+
+                        var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),
+                            dl=l!='dataLayer'
+                                ? '&l='+l
+                                : '';
+
+                        j.async=true;
+                        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+                        f.parentNode.insertBefore(j,f);
+                    })(
+                        window,
+                        document,
+                        'script',
+                        'dataLayer',
+                        'GTM-MRT465Q3'
+                    );
+                `}
+            </Script>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
