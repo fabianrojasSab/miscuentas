22680bcd0b21a55eea5e29258d6a3241285a68a8
Fabian Rojas <fabianrojas.sab@gmail.com>
2026-09-13 13:58:19 -0500

    fix: creacion de banco
    Se realiza modificacion para que permita ingresar nombres de banco con el mismo nombre, agregando un caracter aleaterio para que no quede duplicado en la bd

--- src/components/form_bank_accounts.tsx
+++ src/components/form_bank_accounts.tsx
@@ -124,13 +124,21 @@ export const FormBankAccountUser = ({createBankAccount, bankAccontToEdit, Update
 
     //funcion para encriptar el nombre del banco y convertirlo en un hash
     async function encryptBankName(bankName: string): Promise<string> {
+        const randomArray = new Uint32Array(1);
+        crypto.getRandomValues(randomArray);
+
+        const randomNumber = randomArray[0];
+
+        const value = `${bankName}-${randomNumber}`;
+
         const encoder = new TextEncoder();
-        const data = encoder.encode(bankName);
-        return crypto.subtle.digest("SHA-256", data).then((hash) => {
-            return Array.from(new Uint8Array(hash))
-                .map((b) => b.toString(16).padStart(2, "0"))
-                .join("");
-        });
+        const data = encoder.encode(value);
+
+        const hash = await crypto.subtle.digest("SHA-256", data);
+
+        return Array.from(new Uint8Array(hash))
+            .map((b) => b.toString(16).padStart(2, "0"))
+            .join("");
     }
 
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
